93757577838eaedf056a05360b76162444b36cbe
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2025-04-03 00:56:22 -0300

    Update AluguelController.java

--- java-apirest-main/src/main/java/restiapi/projeto1/controller/AluguelController.java
+++ java-apirest-main/src/main/java/restiapi/projeto1/controller/AluguelController.java
@@ -1,5 +1,6 @@
 package restiapi.projeto1.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,18 +20,21 @@ public class AluguelController {
         this.aluguelService = aluguelService;
     }
 
+    @Operation(summary = "Recupera informações de um registro especifico de aluguel da Base de Dados atraves do id do documento.")
     @GetMapping("/{idAluguel}")
     public ResponseEntity<Aluguel> findById(@PathVariable("idAluguel")Long idAluguel) {
         var aluguel = aluguelService.findById(idAluguel);
         return ResponseEntity.ok(aluguel);
     }
 
+    @Operation(summary = "Deleta um registro de aluguel da Base de Dados")
     @DeleteMapping("/{idAluguel}")
     public ResponseEntity<Aluguel> deleteById(@PathVariable("idAluguel")Long idAluguel) {
         var aluguelDeleted = aluguelService.deleteById(idAluguel);
         return ResponseEntity.ok(aluguelDeleted);
     }
 
+    @Operation(summary = "Adiciona um novo registro de aluguel na Base de Dados")
     @PostMapping
     public ResponseEntity<Aluguel> create(@RequestBody Aluguel aluguelToCreate) {
         var aluguelCreated = aluguelService.create(aluguelToCreate);
@@ -38,9 +42,11 @@ public class AluguelController {
                 .path("/{idAluguel}")
                 .buildAndExpand(aluguelCreated.getIdAluguel())
                 .toUri();
+
         return ResponseEntity.created(location).body(aluguelCreated);
     }
 
+    @Operation(summary = "Atualiza informações de um registro de aluguel da Base de Dados")
     @PutMapping("/{idAluguel}")  // ID na URL
     public ResponseEntity<Aluguel> atualizarAluguel(@PathVariable Long idAluguel, @RequestBody Aluguel aluguelToUpdate) {
         // Validação de consistência
