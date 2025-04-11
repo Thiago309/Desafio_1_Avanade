721dfeb0ea519bbc7b59ea1aaaaaa48547953117
Thiago309 <116599158+Thiago309@users.noreply.github.com>
2025-04-11 11:47:30 -0300

    Adicionando Comentarios

--- java-apirest-main/src/main/java/restiapi/projeto1/Config/SwaggerConfig.java
+++ java-apirest-main/src/main/java/restiapi/projeto1/Config/SwaggerConfig.java
@@ -1,9 +1,17 @@
 package restiapi.projeto1.Config;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+/**
+ * Este código configura a documentação da API utilizando o Springdoc OpenAPI, que integra o Swagger 2.8.5
+ * ao Spring Boot de forma automática.
+ *
+ * @author Thiago Vinicius
+ * @Version 3.0
+ * @since 10/04/2025
+*/
 
 @Configuration
 public class SwaggerConfig {
@@ -12,7 +20,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Minha API - Gerenciador de Alugueis")
+                        .title("API - Gerenciador de Alugueis")
                         .version("3.0.0")
                         .description("Registros de Alugueis de Automoveis"));
     }
